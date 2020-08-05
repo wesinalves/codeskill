f8fb021d5f46f54786f9c429c10a060679aac25f
wesin alves <wesinec@gmail.com>
2020-08-05 16:39:08 -0300

    start plates solution

---
+++ 2020/kickstart/Round A/plates.py
@@ -0,0 +1,14 @@
+'''
+n -> stacks
+k -> plates
+p -> chosen plate
+'''
+
+cases = int(input())
+for t in range(cases):
+    n, k, p = list(map(int, input().split()))
+    for i in range(n)
+        beauty_values = list(map(int, input().split()))
+        maximum_total_sum
+
+    print('Case #{}: {}'.format(t+1, maximum_total_sum))
