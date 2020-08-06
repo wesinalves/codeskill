f5f40cd7f515d1b89f3c8e323e57f8e9a394930e
wesin alves <wesinec@gmail.com>
2020-08-06 16:40:41 -0300

    update plates

--- 2020/kickstart/Round A/plates.py
+++ 2020/kickstart/Round A/plates.py
@@ -5,23 +5,24 @@ p -> chosen plate
 '''
 
 cases = int(input())
-beauty_values = []
+# def possible_solution(matrix_values, num_stacks, num_plates, plates):
+#     '''Take all possible solution for P plates'''
+#     solutions = []
 
-def possible_solution(matrix_values, num_stacks, num_plates, plates):
-    '''Take all possible solution for P plates'''
-    solutions = []
-
-    for i in range(num_stacks):
-        for j in range(num_plates):
+#     for i in range(num_stacks):
+#         for j in range(num_plates):
 
 
-    return solutions
+#     return solutions
 
 for t in range(cases):
     n, k, p = list(map(int, input().split()))
-    for i in range(n)
+    beauty_values = []
+    for i in range(n):
         beauty_values.append(list(map(int, input().split())))
 
-    maximum_total_sum = max(possible_solution(beauty_values, n, k, p))
+    #maximum_total_sum = max(possible_solution(beauty_values, n, k, p))
+    print(len(beauty_values), len(beauty_values[0]))
 
-    print('Case #{}: {}'.format(t+1, maximum_total_sum))
+
+    #print('Case #{}: {}'.format(t+1, maximum_total_sum))
