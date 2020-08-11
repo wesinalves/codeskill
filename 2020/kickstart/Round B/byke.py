d610c68183a00491a294d679a5e11bafdf06d404
WesinAlves <wesinec@gmail.com>
2020-08-11 06:44:57 -0300

    Add byke tour problem fo round B 2020

---
+++ 2020/kickstart/Round B/byke.py
@@ -0,0 +1,15 @@
+'''
+Code for google kickstart
+Round B - Byke Tour
+Wesin Ribeiro
+2020
+'''
+
+def main():
+    '''Main function'''
+    tests = int(input())
+    ans = 0
+    for t in range(tests):
+        print('Case #{}: {}'.format(t+1, ans))
+
+main()
