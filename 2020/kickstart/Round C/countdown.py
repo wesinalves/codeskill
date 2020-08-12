bfe26bda22c261f70c05bea6029ea4002a75792c
wesin alves <wesinec@gmail.com>
2020-08-12 08:59:36 -0300

    prepare files for countdown

---
+++ 2020/kickstart/Round C/countdown.py
@@ -0,0 +1,14 @@
+'''
+Code for google kickstart
+Round C - Countdown
+Wesin Ribeiro
+2020
+'''
+def main():
+    '''Main function'''
+    tests = int(input())
+    for t in range(tests):
+        ans = 0
+        print('Case #{}: {}'.format(t+1, ans))
+
+main()
