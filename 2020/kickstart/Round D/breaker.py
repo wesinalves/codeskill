107ef93bdd803ac896622f1657174ba121e6376e
wesin alves <wesinec@gmail.com>
2020-08-17 16:44:30 -0300

    small changes

--- 2020/kickstart/Round D/breaker.py
+++ 2020/kickstart/Round D/breaker.py
@@ -14,8 +14,15 @@ def main():
     tests = int(input())
     for t in range(tests):
         ans = 0
-        n = int(input().split())
+        n = int(input())
         samples = list(map(int, input().split()))
+        for i in range(1, n-1):
+            if samples[i] > samples[i+1] and samples[i] > samples[i-1]):
+                print(samples[i], samples[i-1], samples[i+1])
+                ans += 1
+
+
+
         print('Case #{}: {}'.format(t+1, ans))
 
 main()
