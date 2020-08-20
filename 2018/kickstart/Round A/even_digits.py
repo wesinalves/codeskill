1542e53e7f0c24b7d372bef014f11610018e9292
wesin alves <wesinec@gmail.com>
2020-08-20 16:26:08 -0300

    prepare round A 2018

---
+++ 2018/kickstart/Round A/even_digits.py
@@ -0,0 +1,26 @@
+'''
+Code for google kickstart
+Round A - Even Digits
+Wesin Ribeiro
+2020
+
+##############
+Description
+##############
+
+##############
+Comments
+##############
+
+'''
+
+def main():
+    '''Main function'''
+    tests = int(input())
+    for t in range(tests):
+        ans = 0
+        n = int(input())
+
+        print('Case #{}: {}'.format(t+1, ans))
+
+main()
