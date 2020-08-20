83b931e25244a558ecec9f61ea3961fcf00f9a49
wesin alves <wesinec@gmail.com>
2020-08-20 16:41:23 -0300

    add comments

--- 2018/kickstart/Round A/even_digits.py
+++ 2018/kickstart/Round A/even_digits.py
@@ -5,12 +5,21 @@ Wesin Ribeiro
 2020
 
 ##############
-Description
+Problem
 ##############
+This calculator only has a display, a plus button, and a minus button.
+The integer N is displayed on the calculator display.
+Pressing the plus button increases the current number displayed on the calculator
+display by 1. Similarly, pressing the minus button decreases the current number
+displayed on the calculator display by 1.
+The calculator does not display any leading zeros.
+Determine the minimum number of button presses to make the calculator
+display an integer with no odd digits.
 
 ##############
 Comments
 ##############
+a number is even if the module operation by 2 is 0.
 
 '''
 
@@ -19,6 +28,9 @@ def main():
     tests = int(input())
     for t in range(tests):
         ans = 0
+        button_inscrease = 0
+        button_decrease = 0
+
         n = int(input())
 
         print('Case #{}: {}'.format(t+1, ans))
