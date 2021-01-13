8d66ae7e2101e94eefd71582eca2493303b6d0a0
WesinAlves <wesinec@gmail.com>
2021-01-13 11:19:36 -0300

    refactoring code

--- hackerrank/python/ordered_dict.py
+++ hackerrank/python/ordered_dict.py
@@ -6,21 +6,23 @@ Wesin Ribeiro
 ###################################
 An OrderedDict is a dictionary that remembers the order of the keys that were inserted first. 
 If a new entry overwrites an existing entry, the original insertion position is left unchanged.
-''' 
-
+'''
 from collections import OrderedDict
 
-N = int(input())
-ordered_items = OrderedDict()
+def main():
+    '''Main Function'''
+    N = int(input())
+    ordered_items = OrderedDict()
 
-for i in range(N):
-    line = input().split(' ')
-    item_name = ' '.join(line[0:-1])
-    price = int(line[-1])    
-    if ordered_items.get(item_name) == None:
-        ordered_items[item_name] = price
-    else:
-        ordered_items[item_name] += price
-        
-for key, value in ordered_items.items():
-    print(key,value)
+    for _ in range(N):
+        line = input().split(' ')
+        item_name = ' '.join(line[0:-1])
+        price = int(line[-1])
+        if not ordered_items.get(item_name):
+            ordered_items[item_name] = price
+        else:
+            ordered_items[item_name] += price
+    for key, value in ordered_items.items():
+        print(key, value)
+
+main()
